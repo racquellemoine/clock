51728ef2032147b3df5d99177386fa5e4d95722b
racquellemoine <95442623+racquellemoine@users.noreply.github.com>
2024-01-15 21:12:58 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -32,7 +32,7 @@ function drawDot(x, y, radius, color) {
 function drawDots(numDots) {
   
     for (let i = 0; i < numDots; i++) {
-        const x = 0 * canvas.width;
+        const x = 10 * canvas.width;
         const y = i * canvas.height;
         const radius = Math.random() * 10 + 5; // Random radius between 5 and 15
         const color = `rgba(${Math.random() * 255},${Math.random() * 255},${Math.random() * 255},1)`;
